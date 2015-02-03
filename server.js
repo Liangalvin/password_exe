fc98e21cc0a81aa91ded897c732d2167ffe40a22
Alvin Liang <liangalvin@yahoo.com>
2015-02-03 10:17:30 -0500

    updated with json file for passwords

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+
 app.use(express.static(__dirname+'/public'));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var secret = require('./secret.json');
 
 app.get("/", function(req,res){
   res.sendFile(__dirname + '/public/index.html');
 });
 
-var secret = "penguin";
+// var secret = "penguin";
 
 app.get("/login", function(req, res){
-  if(req.query.password === secret){
+  if(req.query.password === secret['password']){
     res.redirect('/secret_page')
   }
   else{
