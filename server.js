70069cce192c880e0a2cc3485940d3cd17d9c8d5
Alvin Liang <liangalvin@yahoo.com>
2015-02-03 09:59:02 -0500

    passwords exe

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+var secret = "penguin";
+
+app.get("/login", function(req, res){
+  if(req.query.password === secret){
+    res.redirect('/secret_page')
+  }
+  else{
+    res.redirect('/')
+  };
+});
+
+app.get('/secret_page', function(req,res){
+  res.sendFile(__dirname + '/public/secret.html')
+});
+
+app.listen(3000);
+console.log("listening on port 3000");
